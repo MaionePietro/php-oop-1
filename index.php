886f6dd2f18d732865ec69c62dcb7f08117745eb
MaionePietro <pietromaionespam@gmail.com>
2023-03-30 19:23:00 +0200

    fine

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
 require_once __DIR__ . "/models/Movie.php";
 
-$film1 = new Movie("titolo", "23");
+$film1 = new Movie("Star Wars", "23", "bla bla bla");
 //$a = Movie::movieTypology[1];
-var_dump($Movie);
+//var_dump($film1);
+//var_dump($film1->lenght());
 ?>
 
 <!DOCTYPE html>
@@ -13,8 +14,17 @@ var_dump($Movie);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>php-oop-1</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
 </head>
-<body>
-    
+<body class="vh-100 d-flex justify-content-center align-items-center">
+    <div class="container">
+        <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <!-- <p class="card-text"> <?php //echo $film1->concatena(); ?></p> -->
+            </div>
+        </div>
+    </div>
 </body>
 </html>
--- models/Movie.php
+++ models/Movie.php
@@ -1,16 +1,22 @@
 <?php
 class Movie{
-    public $title;
+    public string $title;
     public int $duration;
-    public $genere;
+    public string $genere;
+    public string $description;
     //public static $movieTypology = ["action", "fantasy", "motivational"];
 
-    function __construct($_title, $_duration){
+    function __construct($_title, $_duration, $_description){
         $this->title = $_title;
         $this->duration = $_duration;
+        $this->$description = $_description;
     }
-    
-    public function getFullAddress(){
 
+    public function concatena(){
+        $text ="$this->title, $this->duration";
+        if(strlen($this->description) < 10 ){
+            $text .= $this->$description;
+        }
+        return $text;
     }
 }
